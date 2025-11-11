5549a828b7ea8c8861e50f73e51d3bfc11f1af9c
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-11-11 20:08:53 +0530

    added code for digit 14-99

---
+++ tens.py
@@ -0,0 +1,12 @@
+from ds import ds as d
+
+def tens(x:int)-> str:
+  # 14-19
+  if x >13 and x<20:
+    x %= 10 #14 %10 = 4
+    return f"{d[x]}teen"
+  #21-99 except 10 multiples 
+  elif x >20 and x%10 !=0 and x <100:
+    fd = x // 10 #23 // 10 = 2 
+    ld =  x%10 #23 % 10 = 3
+    return f"{d[fd]}-{d[ld]}"
