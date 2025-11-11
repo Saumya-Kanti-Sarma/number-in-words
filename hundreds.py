1bd86de4e257a29222d03d251aed1cd9a256a47f
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-11-11 20:27:02 +0530

    added hundreds function

---
+++ hundreds.py
@@ -0,0 +1,9 @@
+from ds import ds as d
+
+def hundreds(x:int) -> str:
+  n1 = x //100 # 132 // 100 =1
+  x %= 100 #132 %100 = 32
+  n2 = (x//10)*10 #(32 //10)*10= 30
+  x %= 10 # 32 % 10 = 2
+
+  return f"{d[n1]} hundred {d[n2]+' ' if n2 != 0 else ''}{d[x]+' ' if x != 0 else ''}"  
