19ad1bebe1ae8689d75f76963f2bd0cd62d84b45
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-01-18 12:52:14 +0530

    changed hundreds logic

--- hundreds.py
+++ hundreds.py
@@ -1,11 +1,7 @@
-from .ds import ds as d
+from ds import ds as d
+from Tens import tens
 
 def hundreds(x:int) -> str:
-  if x in d:
-    return d[x]
   n1 = x //100 # 132 // 100 =1
-  x %= 100 #132 %100 = 32
-  n2 = (x//10)*10 #(32 //10)*10= 30
-  x %= 10 # 32 % 10 = 2
-
-  return f"{d[n1]} hundred {d[n2]+' ' if n2 != 0 else ''}{d[x]+' ' if x != 0 else ''}"  
+  n2 = x % 100 # 132 % 100 = 32
+  return f"{d[n1]} hundred {tens(n2) if n2 !=0 else ""}"  
