d9cfdfe8478aaf993f16a73cc82ee2bd5711b50a
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2026-01-20 12:52:46 +0530

    deleted old structure

--- hundreds.py
+++
@@ -1,7 +0,0 @@
-from ds import ds as d
-from Tens import tens
-
-def hundreds(x:int) -> str:
-  n1 = x //100 # 132 // 100 =1
-  n2 = x % 100 # 132 % 100 = 32
-  return f"{d[n1]} hundred {tens(n2) if n2 !=0 else ""}"  
--- tens.py
+++
@@ -1,9 +0,0 @@
-from ds import ds as d
-
-def tens(x:int)-> str:
-  if x in d:
-    return d[x]
-  elif x >20 and x <100:
-    fd = (x // 10)*10 #23 // 10 = 2 
-    ld =  x%10 #23 % 10 = 3
-    return f"{d[fd]} {d[ld]}"
